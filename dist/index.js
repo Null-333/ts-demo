23bfc7d29abc6235fe18ae900351149e9ce9440e
Helen <yinhelen.hlj@qq.com>
2022-03-09 14:22:04 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function helloName(name) {
+    const str = 'hello';
+    console.log(`${str} ${name}`);
+}
+helloName('helen');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function helloName(name: string) {
+    const str = 'hello';
+    console.log(`${str} ${name}`);
+}
+helloName('helen');
